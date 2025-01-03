79567a57397b6698b785be28cdc6e498855a72cb
Cybergh1 <gamelessdean11@gmail.com>
2025-01-03 00:52:55 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 const settings = {
   packname: 'My Sticker Pack',
   author: 'My Bot',
-  botName: "Knight Bot",
+  botName: "Cyberbot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  ownerNumber: '508591189', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "1.0.0",
+  version: "1.0.1",
 };
 
 module.exports = settings;
